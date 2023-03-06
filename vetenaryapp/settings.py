6fac90b80fefada08c3b73511ca67b80e691a37d
Henry Cheruiyot <matranic@gmail.com>
2023-03-06 18:44:53 +0300

    dsdsdsd

--- vetenaryapp/settings.py
+++ vetenaryapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-va4xwyr_@h6_aku^3vr%5%@^34xzl$gz^xsvug^7y0x_lx*h^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +128,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
