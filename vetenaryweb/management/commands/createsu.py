90f1383f367b66ffd314460465b5dd413719a77f
Henry Cheruiyot <matranic@gmail.com>
2023-03-06 19:05:47 +0300

    manae

---
+++ vetenaryweb/management/commands/createsu.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                password='henry5765'
+            )
+        print('Superuser has been created.')
